5ad47327ea776b32d853b1c1ea12a3a11d987d9c
notsammy002 <ak0020302@gmail.com>
2022-03-17 16:37:20 +0530

    project are added in the portfolio

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -11,76 +11,77 @@ import eight from '../assets/svg/projects/eight.svg'
 export const projectsData = [
     {
         id: 1,
-        projectName: 'Employee Management',
-        projectDesc: 'This project aims to build an employee management system that consolidates all information of a company',
+        projectName: 'Facebook (clone)',
+        projectDesc: 'In this project we have tried to make a look alike clone of Facebook. With our efforts and the technology stack, that we have learned so far in the masai school, we were able to clone the front end with high precision and quality.',
         tags: ['React', 'CSS', 'Material Ui'],
-        code: 'https://github.com/notsammy002',
-        demo: 'https://github.com/notsammy002',
+        code: 'https://github.com/notsammy002/Facebook-clone',
+        demo: 'https://facebook-clone-three-orpin.vercel.app/login',
         image: one
     },
     {
         id: 2,
-        projectName: 'Railway Tracking',
-        projectDesc: 'This project is a railway tracking system that allows users to obtain relevant information about train timing.',
-        tags: ['Django', 'Tailwind CSS'],
-        code: 'https://github.com/notsammy002',
-        demo: 'https://github.com/notsammy002',
+        projectName: 'Electric-Car',
+        projectDesc: 'Front-end Website based on Electric Cars.',
+        tags: ['HTML', 'CSS', 'JS'],
+        code: 'https://github.com/notsammy002/stage-1-project',
+        demo: 'https://electric-car-project.netlify.app/',
         image: two
     },
     {
         id: 3,
-        projectName: 'Weather App',
-        projectDesc: 'Weather forecast systems and applications predict weather conditions based on multiple parameters.',
-        tags: ['Django', 'CSS', 'Material Ui'],
-        code: 'https://github.com/notsammy002',
-        demo: 'https://github.com/notsammy002',
+        projectName: 'Travel_clone',
+        projectDesc: 'A fully responsive website that provides Rooms and dining, Named #TravelKaro',
+        tags: ['HTML', 'CSS', 'JS'],
+        code: 'https://github.com/notsammy002/Travel_clone-1',
+        demo: 'https://bhaskarkrp.github.io/Travel_clone/',
         image: three
     },
     {
         id: 4,
-        projectName: 'Android Patient Tracker',
-        projectDesc: 'This project involves the development of an Android application for viewing and managing patient data.',
-        tags: ['Flutter', 'Firebase'],
-        code: 'https://github.com/notsammy002',
-        demo: 'https://github.com/notsammy002',
-        image: four
+        projectName: 'Todo App',
+        projectDesc: 'A html site create to take input from user and display it order wise',
+        tags: ['HTML', 'CSS', 'JS'],
+        code: 'https://github.com/notsammy002/todo_list_html',
+        demo: 'https://github.com/notsammy002/todo_list_html',
+        image: eight
     },
     {
         id: 5,
-        projectName: 'E-Commerce App',
-        projectDesc: 'A Simple E-commerce application',
-        tags: ['React Native', 'Firebase'],
-        code: 'https://github.com/notsammy002',
-        demo: 'https://github.com/notsammy002',
-        image: five
+        projectName: 'Authentication page',
+        projectDesc: 'It is project is which the dummy login and logout page are created the user can only access it when he/she enter the correct user and password',
+        tags: ['React', 'Redux'],
+        code: 'https://github.com/notsammy002/redux-auth-page',
+        demo: 'https://auth-user-page.herokuapp.com/',
+        image: four
     },
     {
         id: 6,
-        projectName: 'Uber Lite',
-        projectDesc: 'Uber clone',
-        tags: ['Flutter'],
-        code: 'https://github.com/notsammy002',
-        demo: 'https://github.com/notsammy002',
-        image: six
+        projectName: 'Ecommerce Website',
+        projectDesc: 'It is dummy Ecommerce website which has the variety of products displayed whose data is fetched from an api.',
+        tags: ['React'],
+        code: 'https://github.com/notsammy002/product-page-react',
+        demo: 'https://online-store-masai.herokuapp.com/',
+        image: five
     },
     {
         id: 7,
-        projectName: 'Stock Market App',
-        projectDesc: 'A simple stock market API app',
-        tags: ['React', 'Redux', 'Bootstrap'],
-        code: 'https://github.com/notsammy002',
-        demo: 'https://github.com/notsammy002',
-        image: seven
+        projectName: 'Signup page',
+        projectDesc: 'A mockup of of a singup and signin',
+        tags: ['React'],
+        code: 'https://github.com/notsammy002/signup-page-react',
+        demo: 'https://signup-page-masai.netlify.app/',
+        image: six
     },
     {
         id: 8,
-        projectName: 'Car Pooling System',
-        projectDesc: 'The carpooling system merges multiple people in a car which leads to meet new people, reduces pollution',
-        tags: ['Flutter', 'React'],
-        code: 'https://github.com/notsammy002',
-        demo: 'https://github.com/notsammy002',
-        image: eight
+        projectName: 'Stopwatch',
+        projectDesc: 'Stopwatch is create the high order function in javascript',
+        tags: ['React'],
+        code: 'https://github.com/notsammy002/stopwatch-react',
+        demo: 'https://github.com/notsammy002/stopwatch-react',
+        image: seven
     },
+    
 ]
 
 
